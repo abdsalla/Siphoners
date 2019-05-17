6430eff3d5ffc221c91a144461871ce6d110b3b3
Duli <abdsalla@uat.edu>
2019-05-17 15:43:51 -0700

    Setup Debug mode and Fixed UV Sensor functionality
    debug mode is a boolean that displays values if hooked up correctly and UV sensor should be fixed in theory

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public IUVSensor sensor;
+
 
     void Awake()
     {
@@ -17,9 +19,12 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        sensor = GetComponent<IUVSensor>();
+
     }
     void Update()
     {
-
+        sensor.GetUVValue();
     }
 }
---
+++ Assets/Scripts/TestUVSensor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TestUVSensor : MonoBehaviour, IUVSensor
+{
+
+    public Text textBox;
+    
+    public void Update()
+    {
+        if (textBox != null)
+        {
+            textBox.text = "" + GetUVValue();
+        }
+    }
+
+    public float GetUVValue()
+    {
+        return Random.Range(0,128);
+    }
+
+}
