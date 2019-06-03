89bef2ebf4d1a3540a69c5773e5bdee770fadf9b
Darin <darpaler@uat.edu>
2019-06-02 21:44:50 -0700

    Added Inventory, Items, and PickUps

---
+++ Assets/Scripts/Items/PickUp_Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp_Item : PickUp
+{
+    public Item item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnPickUp(PlayerMovement player)
+    {
+        player.GetComponent<Inventory>().AddItem(item);
+        base.OnPickUp(player);
+    }
+
+}
