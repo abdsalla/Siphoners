c3f18e15c436beb0247159341a1bdca438290017
Duli <abdsalla@uat.edu>
2019-06-14 00:31:34 -0700

    Quest template setup in separate scene

--- Assets/Scripts/UI/QuestTemplate.cs
+++ Assets/Scripts/UI/QuestTemplate.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine.UI;
 using UnityEngine;
 
 [CreateAssetMenu (fileName = "New Quest", menuName = "Quest" )]
-public class QuestTemplate : ScriptableObject
+public class QuestTemplate : ScriptableObject 
 {
     public new string name;
     public string description;
     public string reward;
     public Image questPopUp;
-
 }
