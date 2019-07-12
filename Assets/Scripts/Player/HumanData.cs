46865fb6e7a57abcb0ccbfa7a570a9e069488a19
Darin <Darpaler@uat.edu>
2019-07-12 12:49:58 -0700

    Added Human Data, Fixed Camera

---
+++ Assets/Scripts/Player/HumanData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanData : MonoBehaviour
+{
+    //Variables
+    public Transform viewPoint;
+
+}
