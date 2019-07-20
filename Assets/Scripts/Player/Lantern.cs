bf5b0f93606a56b9814264eb50863bd6a421127e
Duli <abdsalla@uat.edu>
2019-07-19 17:07:10 -0700

    Lantern Particle effect now uses energy

--- Assets/Scripts/Player/Lantern.cs
+++ Assets/Scripts/Player/Lantern.cs
@@ -11,24 +11,27 @@ public class Lantern : MonoBehaviour
     public GameObject playerReference;
 
     private bool isOn;
+    private ParticleSystem lightParticle;
     private Light lightComp;
     private Energy energyRef;
 
 
     void Start()
     {
-    lightComp = lightGameObject.AddComponent<Light>();
-    lightComp.type = LightType.Spot;
-    lightComp.lightmapBakeType = LightmapBakeType.Mixed;
+    lightComp = lightGameObject.GetComponent<Light>();
+    //lightComp.type = LightType.Spot;
+    lightComp.lightmapBakeType = LightmapBakeType.Realtime;
     energyRef = playerReference.GetComponent<Energy>();
-    lightComp.color = Color.yellow;
+    lightParticle = lightGameObject.GetComponent<ParticleSystem>();
+    var lights = lightParticle.lights;
+    //lightComp.color = Color.yellow;
     isOn = true;
     
     }
 
     void Update()
     {
-        Debug.Log("energyRef.solarCharged = " + energyRef.solarCharged);
+       // Debug.Log("energyRef.solarCharged = " + energyRef.solarCharged);
         if (energyRef.energyBar.value > 0 && isOn == true)
         {
             energyRef.UseEnergy(energyDepletionRate);
@@ -50,26 +53,28 @@ public class Lantern : MonoBehaviour
 
     void TurnOn()
     {
-        lightComp.enabled = true;
+        var lights = lightParticle.lights;
+        lights.enabled = true;
         isOn = true;
     }
 
     void TurnOff()
     {
-        lightComp.enabled = false;
+        var lights = lightParticle.lights;
+        lights.enabled = false;
         isOn = false;
     }
 
     void SolarCharged()
     {
         Debug.Log("Solar Charged");
-        lightComp.intensity = 7;
+       // lightComp.intensity = 7;
 
     }
 
     void SolarRevert()
     {
-        lightComp.intensity = 1;
+       // lightComp.intensity = 1;
     }
 
 }
