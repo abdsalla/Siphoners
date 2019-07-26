1252330f2a1822d3a86c1e11ea923a56af706205
Duli <abdsalla@uat.edu>
2019-07-26 16:20:15 -0700

    Lighting Particle effect setup

---
+++ Assets/Scripts/Lighting/FogDissipate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FogDissipate : MonoBehaviour
+{
+    private ParticleCollisionEvent dissipate;
+    private ParticleSystem fog;
+
+    public GameObject fogEmitter;
+
+    void Start()
+    {
+        fog = fogEmitter.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject fogEmitter)
+    {
+        if (fog.gameObject.tag == "Fog")
+        {
+            Destroy(fog);
+        }
+    }
+
+}
